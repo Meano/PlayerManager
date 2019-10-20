2a66d9ff808e839de6554fa085012ccbe97ff023
Meano <meano@foxmail.com>
2019-10-20 18:25:11 +0800

    Use JedisPool solved aysnc Broken Pipe error

--- src/net/meano/PlayerManager/PlayerInfo.java
+++ src/net/meano/PlayerManager/PlayerInfo.java
@@ -1,16 +1,35 @@
 package net.meano.PlayerManager;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import redis.clients.jedis.Jedis;
 
 public class PlayerInfo {
 
 	public BukkitMain PMBukkit;
 	public BungeeMain PMBungee;
 	
+	private Date LoginTime = new Date();
+	
+	public void setLoginTime() {
+		LoginTime = new Date();
+	}
+	
+	public Date getLoginTime() {
+		return LoginTime;
+	}
+	
+	public long getOnlineTime() {
+		return (new Date()).getTime() - LoginTime.getTime();
+	}
+
+	public JedisPool RedisPool;
+
 	public void LogInfo(String msg) {
 		if(PMBukkit != null)
 			PMBukkit.getLogger().info(msg);
@@ -20,18 +39,18 @@ public class PlayerInfo {
 	}
 	
 	private Map<String, Object> PlayerInfoMap = new HashMap<String, Object>();
-	private Jedis Redis; 
 	
-	PlayerInfo(ResultSet playerData, Jedis redis, Map<String, String> DataClass) {
+	PlayerInfo(ResultSet playerData, Map<String, String> DataClass) {
 		try {
 			Class.forName("org.bukkit.plugin.java.JavaPlugin");
 			PMBukkit = BukkitMain.Instance;
+			RedisPool = PMBukkit.RedisPool;
 		} catch (ClassNotFoundException e) {
 			LogInfo("未找到Bukkit驱动" + e.getLocalizedMessage());
 			PMBungee = BungeeMain.Instance;
+			RedisPool = PMBungee.RedisPool;
 		}
 
-		Redis = redis;
 		for(Map.Entry<String, String> ClassEntry : DataClass.entrySet() ) {
 			switch(ClassEntry.getValue()) {
 				case "String":
@@ -86,17 +105,31 @@ public class PlayerInfo {
 		return GetString("PlayerName");
 	}
 
+	public String GetGlobalRedisKey(String keyName) {
+		Jedis redis = RedisPool.getResource();
+		String globalRedisKey = redis.get(keyName);
+		redis.close();
+		return globalRedisKey;
+	}
+
 	public String GetRedisKey(String keyName) {
-		return Redis.get(keyName + "." + GetDataUUID());
+		Jedis redis = RedisPool.getResource();
+		String redisKey = redis.get(keyName + "." + GetDataUUID());
+		redis.close();
+		return redisKey;
 	}
 	
 	public void SetRedisKey(String keyName, String value) {
-		Redis.set(keyName + "." + GetDataUUID(), value);
+		Jedis redis = RedisPool.getResource();
+		redis.set(keyName + "." + GetDataUUID(), value);
+		redis.close();
 	}
 	
 	public void SetRedisKey(String keyName, String value, int expire) {
-		Redis.set(keyName + "." + GetDataUUID(), value);
-		Redis.expire(keyName + "." + GetDataUUID(), expire);
+		Jedis redis = RedisPool.getResource();
+		redis.set(keyName + "." + GetDataUUID(), value);
+		redis.expire(keyName + "." + GetDataUUID(), expire);
+		redis.close();
 	}
 	
 	private String GetResultString(ResultSet playerData, String KeyName){
@@ -106,8 +139,8 @@ public class PlayerInfo {
 			return "";
 		}
 	}
-	
-	private Integer GetResultInteger(ResultSet playerData,String KeyName) { 
+
+	private Integer GetResultInteger(ResultSet playerData, String KeyName) { 
 		try {
 			return playerData.getInt(KeyName);
 		} catch (SQLException e) {
@@ -115,7 +148,7 @@ public class PlayerInfo {
 		}
 	}
 	
-	private Boolean GetResultBoolean(ResultSet playerData,String KeyName) { 
+	private Boolean GetResultBoolean(ResultSet playerData, String KeyName) { 
 		try {
 			return playerData.getBoolean(KeyName);
 		} catch (SQLException e) {
